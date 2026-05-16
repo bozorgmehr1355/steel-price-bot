96e196437b2d4d4f7889453cdc4d43e4f9a85d1e
Mohamad Reza Bozorgmehr <36222603+bozorgmehr1355@users.noreply.github.com>
2026-05-16 11:16:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ from bs4 import BeautifulSoup
 # CONFIGURATION
 # ═══════════════════════════════════
 
+
 TOKEN = os.environ.get("BOT_TOKEN")
 METALPRICE_API_KEY = os.environ.get("METALPRICE_API_KEY")
 ADMIN_ID = 715854466
